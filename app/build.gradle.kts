026534bfadcbb11d2d20246d318dd6c97818774d
Minh Cat DO <m.do@myskolae.fr>
2026-04-24 22:59:17 +0200

    feat: add dep retrofit and gson

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,10 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    // Pour les Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
 }
