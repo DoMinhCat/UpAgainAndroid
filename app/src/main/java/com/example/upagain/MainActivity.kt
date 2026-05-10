c0ff4c2107b72504ceee874dc73e05f6c8d3e510
Minh Cat DO <m.do@myskolae.fr>
2026-05-10 22:25:07 +0200

    feat: navigation logic for bottom nav bar

--- app/src/main/java/com/example/upagain/MainActivity.kt
+++ app/src/main/java/com/example/upagain/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -20,11 +21,50 @@ class MainActivity : AppCompatActivity() {
         bottomNav = findViewById(R.id.bottom_nav)
         bottomNav?.itemIconTintList = null
 
+        // 1. Set the default fragment on first load
+        if (savedInstanceState == null) {
+            replaceFragment(ShopFragment()) // TODO: change to home dashboard
+        }
+
+        // 2. Set the listener for clicks
+        bottomNav?.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_shop -> {
+                    replaceFragment(ShopFragment())
+                    true
+                }
+                R.id.nav_container -> {
+                    replaceFragment(ContainerFragment())
+                    true
+                }
+                R.id.nav_dashboard -> {
+                    replaceFragment(ContainerFragment())
+                    true
+                }
+                R.id.nav_community -> {
+                    replaceFragment(PostFragment())
+                    true
+                }
+                R.id.nav_profile -> {
+                    replaceFragment(ShopFragment()) // TODO: change to home dashboard
+                    true
+                }
+                else -> false
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(mainView) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             bottomNav?.updatePadding(bottom = systemBars.bottom)
             insets
         }
+    }
+
+    // Helper function to handle the transaction of replacing fragment
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
