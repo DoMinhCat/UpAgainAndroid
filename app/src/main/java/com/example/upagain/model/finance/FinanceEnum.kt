34841a378752f9dd7a739cdd1e10030e4f1d4068
Minh Cat DO <m.do@myskolae.fr>
2026-07-06 01:46:58 +0200

    fix(book ads): finance key enum

--- app/src/main/java/com/example/upagain/model/finance/FinanceEnum.kt
+++ app/src/main/java/com/example/upagain/model/finance/FinanceEnum.kt
@@ -2,9 +2,11 @@ package com.example.upagain.model.finance
 
 import com.google.gson.annotations.SerializedName
 
-enum class FinanceKeyEnum(key: String) {
+enum class FinanceKeyEnum(val key: String) {
     @SerializedName("trial_days") TRIAL_DAYS("trial_days"),
     @SerializedName("commission_rate") COMMISSION_RATE("commission_rate"),
     @SerializedName("ads_price_per_month") ADS_PRICE_PER_MONTH("ads_price_per_month"),
     @SerializedName("subscription_price") SUBSCRIPTION_PRICE("subscription_price");
+
+    override fun toString(): String = key
 }
